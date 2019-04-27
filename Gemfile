b7c462cee200aa04ed1148ad219197a42fc0e18e
Kat B <katreesebarnes@gmail.com>
2019-04-27 10:35:10 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in Kat-cli.gemspec
+gemspec
---
+++ lib/Kat/cli.rb
@@ -0,0 +1,8 @@
+require "Kat/cli/version"
+
+module Kat
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
